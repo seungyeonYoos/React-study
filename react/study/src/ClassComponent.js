860af20d317d3e817fcbf97af152a96a325405bf
ttyyree <cjsrnr90218@gmail.com>
2022-10-19 18:56:52 +0900

    react자습

---
+++ react/study/src/ClassComponent.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+class ClassComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.event
+    }
+    render() {
+        const { number } = this.state;
+        return(
+            <>
+                <h3>Number: {number}</h3>
+                <button onClick={()=>{
+                    this.setState({ number: number + 1 });
+                }}>+1</button>
+                <button onClick={() => {
+                    this.setState(prev => {
+                        return {
+                            number: prev.number + 1
+                        };
+                    });
+                }}>2로 설정</button>
+            </>
+        )
+    }
+}
+
+export default ClassComponent;
---
+++ react/study/src/FunctionComponent.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function FunctionComponent() {
+
+    return(
+        <>
+            <button onClick={this.printConsole}>퇴장</button>
+        </>
+    );
+}
+
+export default FunctionComponent;
