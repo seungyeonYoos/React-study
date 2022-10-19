11c5414c07a0ca22da17c65a699ba13e5d4282f5
ttyyree <cjsrnr90218@gmail.com>
2022-10-19 16:58:36 +0900

    react자습

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    const truee = true;
+    const txt = "true입니다리.";
+    return(
+      <>
+        { truee ? (<div>{txt}</div>) : (<div>false입니다.</div>)}
+      </>
+    );
+  }
 }
 
 export default App;
