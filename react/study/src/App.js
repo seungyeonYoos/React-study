c7a8cf8fab92464368f470fda88daf905cfc834b
ttyyree <cjsrnr90218@gmail.com>
2022-10-19 18:57:05 +0900

    react자습

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import ClassComponent from './ClassComponent';
+import FunctionComponent from './FunctionComponent';
 
 class App extends Component{
   render() {
-    const truee = true;
-    const txt = "true입니다리.";
     return(
       <>
-        { truee ? (<div>{txt}</div>) : (<div>false입니다.</div>)}
+        <ClassComponent></ClassComponent>
+        <FunctionComponent></FunctionComponent>
       </>
     );
   }
