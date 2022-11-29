7f31e77db82845c37ea2eba62728fed611ec30c2
ttyyree <cjsrnr90218@gmail.com>
2022-11-30 02:40:38 +0900

    react 공부

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const[toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
@@ -11,9 +12,12 @@ function App() {
       return;
     }
     setToDo("");
-  }
+    setToDos(currentArrey => [toDo, ...currentArrey]);
+  };
+  console.log(toDos);
   return (
     <>
+    <h1>My To Dos</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} 
         value={toDo} 
