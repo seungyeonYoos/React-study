29698a01798fba95435ea1f6a58d4beab80f0e0a
ttyyree <cjsrnr90218@gmail.com>
2022-11-30 05:58:38 +0900

    react 공부

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,14 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [] = useState();
-  useEffect(() => {
-    fetch(
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (
+      await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    ).then((Response) => Response.json()).then(json => console.log(json));
+    )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies()
   }, []);
+  console.log(movies);
   return <>
-    <div>{loading ? <h1>Loading...</h1> : null}</div>
+    <div>
+      {loading ? (<h1>Loading...</h1>
+      ) : (
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}/>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map( g => 
+              <li key={g}>{g}</li>
+              )}
+            </ul>
+          </div>
+        ))}
+      </div>
+      )}
+    </div>
   </>;
 }
 
