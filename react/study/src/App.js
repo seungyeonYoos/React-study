814da01bc7b7013c3d11cb03c63c0708c4741d1b
ttyyree <cjsrnr90218@gmail.com>
2023-02-16 18:24:01 +0900

    react study

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,16 +1,45 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, 글제목변경] = useState(['여자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
 
-  return(
+  return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4 style={ {color : 'red', fontSize : '20px'} }>{ post }</h4>
+      <button onClick={()=>{ 
+        let sort = [...글제목];
+        sort.sort();
+        글제목변경(sort);
+       }}>가나다순정렬</button>
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '남자 코트 추천';
+        글제목변경(copy);
+      }}>button</button>
+
+      <div className='list'>
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <p>2월 15일 발행</p>
+      </div>
+
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 15일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 15일 발행</p>
+      </div>
     </div>
   );
 }
