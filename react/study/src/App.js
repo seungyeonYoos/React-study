4a552bbafc1be0e9797b51a7b547d4287f994a4f
ttyyree <cjsrnr90218@gmail.com>
2023-03-17 21:37:19 +0900

    react) 조건문, 반복문

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -6,9 +6,11 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['여자 코트 추천', '강남 우동 맛집', '파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState('닫힘');
+
+  
 
   return (
     <div className="App">
@@ -27,27 +29,33 @@ function App() {
         글제목변경(copy);
       }}>글수정</button>
 
-      <div className='list'>
-        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
-        <p>2월 15일 발행</p>
-      </div>
+      {
+        글제목.map(function(a, i){
+          return (<div className='list'key={i}>
+          <h4 onClick={()=>{
+          modal == '열림' ? setModal('닫힘'): setModal('열림')
+        }}>{a}  <span onClick={() => {  }}>👍</span> {따봉} </h4>
+          <p>2월 15일 발행</p>
+        </div>)
+        })
+      }
+      {
+        modal == '열림' ? <Modal />: null
+      }
+      
+    </div>
+  );
+}
 
-      <div className='list'>
-        <h4>{글제목[1]}</h4>
-        <p>2월 15일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{글제목[2]}</h4>
-        <p>2월 15일 발행</p>
-      </div>
-
-      <div className='modal'>
+function Modal(){
+  return(
+    <div className='modal'>
         <h4>제목</h4>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
-    </div>
-  );
+  )
 }
+
 
 export default App;
